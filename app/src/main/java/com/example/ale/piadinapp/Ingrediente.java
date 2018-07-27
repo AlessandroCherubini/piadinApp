94244a26d6f0e9339e6ffef51c48fd36f4b97343
Unknown <davidefalleti92@gmail.com>
2018-07-27 19:21:33 +0200

    Aggiunta classe Ingrediente

---
+++ app/src/main/java/com/example/ale/piadinapp/Ingrediente.java
@@ -0,0 +1,40 @@
+package com.example.ale.piadinapp;
+
+public class Ingrediente {
+
+    private String name;
+    private double price;
+
+
+    public Ingrediente(String name, double price){
+
+        this.name=name;
+        this.price=price;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingrediente{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
+
