75635c956178a4c9b4ec125e9e54750435b44a86
Unknown <davidefalleti92@gmail.com>
2018-08-13 11:23:22 +0200

    Creato servizio "funzionante"

--- app/src/main/java/com/example/ale/piadinapp/home/CartActivity.java
+++ app/src/main/java/com/example/ale/piadinapp/home/CartActivity.java
@@ -1,6 +1,8 @@
 package com.example.ale.piadinapp.home;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -52,6 +54,7 @@ import com.example.ale.piadinapp.R;
 import com.example.ale.piadinapp.classi.CartItem;
 import com.example.ale.piadinapp.classi.Ingrediente;
 import com.example.ale.piadinapp.classi.Piadina;
+import com.example.ale.piadinapp.classi.ServiceNotification;
 import com.example.ale.utility.CustomRequest;
 import com.example.ale.utility.VolleyCallback;
 import com.example.ale.utility.VolleySingleton;
@@ -86,6 +89,7 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
     Carteasy cs = new Carteasy();
     Map<Integer, Map> data;
     VolleyCallback durataCallBack;
+    private static Context context;
 
 
     public boolean checkLocationPermission()
@@ -108,6 +112,8 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
         TextView tvTot= (TextView)findViewById(R.id.tv_total);
         Button clearCart = (Button)findViewById((R.id.clear_cart));
 
+        context=getApplicationContext();
+
 
         data = cs.ViewAll(getApplicationContext());
         createNotificationChannel();
@@ -123,8 +129,6 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
         cs.persistData(getApplicationContext(),true);
 
         // ricevo l'elemento inserito nel carrello
-
-
 
         String id;
         if (data == null || data.size()==0) {
@@ -162,6 +166,7 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
                 k++;
 
             }
+
         }
 
 
@@ -211,44 +216,32 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
         Button notify = (Button) findViewById(R.id.effettua_ordine);
         notify.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
+            public void onClick(final View v) {
 
-                durataCallBack = new VolleyCallback() {
-                    @Override
-                    public void onSuccess(String result) {}
-
-                    @Override
-                    public void onSuccessMap(int duration) {
-
-                        Log.d("DISTANZA",""+duration);
-
-                        GregorianCalendar now = new GregorianCalendar();
-                        Date oraAttuale = now.getTime();
-
-                        GregorianCalendar oraRitiro= new GregorianCalendar(2018,Calendar.AUGUST,11,21,00,00);
-                        Date oraRitiroDate = oraRitiro.getTime();
-
-                        long timeBeforePick = TimeUnit.MILLISECONDS.toSeconds(oraRitiroDate.getTime() - oraAttuale.getTime());
-
-                        //TODO IDEARE BENE IL CORPO DEL METODO E CONTROLLARE I TIPI CON DURATION, FORSE LONG?
-                        if (timeBeforePick-((long)duration+TimeUnit.MINUTES.toSeconds(5))==0){}
-
-
-
-
-
-                    }
-                };
-
-                sendNotification();
-
-                Location location = getLocation();
-                final double latitude =location.getLatitude();
-                final double longitude =location.getLongitude();
-                travelTimeRequest(latitude,longitude);
             }
 
         });
+
+        Button startService = (Button) findViewById(R.id.start_service);
+        startService.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(final View v) {
+
+                String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
+                if (EasyPermissions.hasPermissions(getApplicationContext(), perms)) { }
+                else
+                {
+                    EasyPermissions.requestPermissions(getParent(), "Richiesta permesso accesso posizione",1, perms);
+                }
+
+                Log.d("PERMESSI",""+checkLocationPermission());
+
+                startService(v);
+
+            }
+
+        });
+
 
     }
 
@@ -288,9 +281,6 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
             data=null;
         }
 
-
-
-
     }
 
     private void createNotificationChannel(){
@@ -308,143 +298,23 @@ public class CartActivity extends AppCompatActivity implements LocationListener{
         }
     }
 
-    private void sendNotification(){
+    @SuppressLint("MissingPermission")
 
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=GianGusto+Piadineria,+Via+dei+Pioppi,+18,+25080+Molinetto+BS");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        mapIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-        PendingIntent navigatePendingIntent = PendingIntent.getActivity(
-                this, 0, mapIntent,PendingIntent.FLAG_CANCEL_CURRENT);
-
-        Intent notificationIntent = new Intent(getApplicationContext(), MyOrderActivity.class);
-
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        PendingIntent orderIntent = PendingIntent.getActivity(getApplicationContext(), 0,
-                notificationIntent, 0);
-
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "channel_piadina");
-        notification.setSmallIcon(R.mipmap.ic_launcher_round);
-        notification.setContentTitle("Ritira l'ordine!");
-        notification.setContentText("Se vuoi ritirare lordine in orario devi partire a breve");
-        notification.setAutoCancel(true);
-        notification.setContentIntent(orderIntent);
-        notification.addAction(R.drawable.ic_map_black_24dp, "Naviga", navigatePendingIntent).setAutoCancel(true);
-
-        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("channel_piadina",
-                    "Channel title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
-        notificationManager.notify(1, notification.build());
+    public void startService(View v)
+    {
+        Log.d("START","SERVICE: Start Service");
+        startService(new Intent(this,ServiceNotification.class));
+        Intent notificationIntent = new Intent(this, ServiceNotification.class);
+        PendingIntent pintent = PendingIntent.getService(this, 0, notificationIntent, 0);
+        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        // Non viene eseguito esattamente ogni x millis perchè decide android quando attivarlo, si potrebbe considerare
+        //SetExact ma porta ad un consumo più elevato e non ci interessa una precisione al minuto
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),10000, pintent);
     }
 
-
-    public void travelTimeRequest(double latitude, double longitude){
-
-
-        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+latitude+","+longitude+"&destinations=GianGusto+Piadineria,+Via+dei+Pioppi,+18,+25080+Molinetto+BS/@45.496155,10.3505613,17z&key=AIzaSyADSK-zR8PLVu3ZhD1UOI6S6dcB-BSKjnQ";
-
-        CustomRequest jsObjRequest = new CustomRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("JSON", response.toString());
-
-                        try {
-                            JSONArray rowsArray = response.getJSONArray("rows");
-                            JSONObject rows = rowsArray.getJSONObject(0);
-                            JSONArray elements = rows.getJSONArray("elements");
-                            JSONObject leg = elements.getJSONObject(0);
-                            JSONObject durationObject = leg.getJSONObject("duration");
-                            int duration = durationObject.getInt("value");
-
-                            durataCallBack.onSuccessMap(duration);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                if (error instanceof TimeoutError){
-                    Toast.makeText(getApplicationContext(), "TimeOut Error!", Toast.LENGTH_SHORT).show();
-                }else if (error instanceof NoConnectionError) {
-                    Toast.makeText(getApplicationContext(), "NoConnection Error!", Toast.LENGTH_SHORT).show();
-                } else if (error instanceof AuthFailureError) {
-                    Toast.makeText(getApplicationContext(), "Authentication Error!", Toast.LENGTH_SHORT).show();
-                } else if (error instanceof ServerError) {
-                    Toast.makeText(getApplicationContext(), "Server Side Error!", Toast.LENGTH_SHORT).show();
-                } else if (error instanceof NetworkError) {
-                    Toast.makeText(getApplicationContext(), "Network Error!", Toast.LENGTH_SHORT).show();
-                } else if (error instanceof ParseError) {
-                    Toast.makeText(getApplicationContext(), "Parse Error!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
-
-    }
-
-    public Location getLocation(){
-
-
-        String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
-        if (EasyPermissions.hasPermissions(this, perms)) { }
-        else
-        {
-            EasyPermissions.requestPermissions(this, "Richiesta permesso accesso posizione",1, perms);
-        }
-
-        Log.d("PERMESSI",""+checkLocationPermission());
-
-        boolean gps_enabled = false;
-        boolean network_enabled = false;
-
-        LocationManager lm = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-
-        gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-        Location net_loc = null, gps_loc = null, finalLoc = null;
-
-        if (gps_enabled){
-            lm.requestLocationUpdates( LocationManager.GPS_PROVIDER, 1000, 100, this);
-            gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        }
-
-        if (network_enabled) {
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 100, this);
-            net_loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-
-        if (gps_loc != null && net_loc != null) {
-
-            //smaller the number more accurate result will
-            if (gps_loc.getAccuracy() > net_loc.getAccuracy())
-                finalLoc = net_loc;
-            else
-                finalLoc = gps_loc;
-
-        } else {
-
-            if (gps_loc != null) {
-                finalLoc = gps_loc;
-            } else if (net_loc != null) {
-                finalLoc = net_loc;
-            }
-        }
-    return finalLoc;
+    public static Context getAppContext(){
+        return CartActivity.context;
     }
 
 
